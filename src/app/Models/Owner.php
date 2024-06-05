f11d5aaef683c971103ccea0165019f635f4234e
Kano.M <itspapari888@gmail.com>
2024-06-06 00:34:51 +0900

    ownerとshopのリレーションの定義

--- src/app/Models/Owner.php
+++ src/app/Models/Owner.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Owner extends Model
+class Owner extends Authenticatable
 {
-    use HasFactory;
+    use Notifiable;
 
     protected $guard = 'owner';
 
