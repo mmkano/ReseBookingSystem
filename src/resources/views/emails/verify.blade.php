c979dce829f1acc5fd6696b83a4a7dc3f798c9f6
Kano.M <itspapari888@gmail.com>
2024-06-09 11:44:04 +0900

    add:フォームにクラス名を追加し、CSSを更新

--- src/resources/views/emails/verify.blade.php
+++ src/resources/views/emails/verify.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>メールアドレス確認</title>
+    <title>本人確認メール</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css')}}">
     <link rel="stylesheet" href="{{ asset('css/verify.css') }}">
 </head>
 <body>
